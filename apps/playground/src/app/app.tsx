92a0f3d597686a277cb4d6631eb28dc3d78a6490
Duong Le <duongbk1602@gmail.com>
2022-07-17 23:13:46 +0700

    setup tailwind

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -4,10 +4,23 @@ import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="playground" />
-      <div />
-    </>
+    <div>
+      When controlling the flow of text, using the CSS property
+      <span className="inline">display: inline</span>
+      will cause the text inside the element to wrap normally. While using the
+      property <span className="inline-block">display: inline-block</span>
+      will wrap the element to prevent the text inside from extending beyond its
+      parent. Lastly, using the property{' '}
+      <span className="block">display: block</span>
+      will put the element on its own line and fill its parent.
+      <div className="flex items-center">
+        <img alt="App icon" src="favicon.ico" />
+        <div>
+          <strong>Andrew Alfred</strong>
+          <span>Technical advisor</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
